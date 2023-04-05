1a3e9b054b8d62f06290fa3f960ee6bfd7599a64
MattBourgeois <Bourgeoismatthew6@gmail.com>
2023-04-05 10:33:35 -0400

    got it to print out all 3 perks from killer

--- Flask_app/controller/user.py
+++ Flask_app/controller/user.py
@@ -17,5 +17,5 @@ def all_info():
 @app.route('/random_killer')
 def killer_random():
 	x = random.randint(0, 32)
-	m = Killer.GetKillerPerks(f"{x}", 2)
+	m = Killer.GetKillerPerks(f"{x}")
 	return render_template('data.html', m = m)
--- Flask_app/models/deep.py
+++ Flask_app/models/deep.py
@@ -9,10 +9,14 @@ class Killer:
 		self.Perk_description = Perk_Description
 		self.Add_ons = Add_ons
 	
-	def GetKillerPerks(killer_num ,perk_num):
-		m = []
-		for perk in data["Players"]["Killers"][killer_num]["Perks"][perk_num]:
-			m.append(perk)
+	def GetKillerPerks(killer_num):
+		m = ""
+		# for i in range(1, 4, 1):
+		# 	x = random.randint(0, 30)
+		# 	Killer.GetKillerPerks(f"{x}", perk_num)
+
+		for perk in data["Players"]["Killers"][killer_num]["Perks"]:
+			m += perk
 		print(m)
 		return m
 		# return data["Players"]["Killers"]["0"]["Perk_Description"][perk_num]
