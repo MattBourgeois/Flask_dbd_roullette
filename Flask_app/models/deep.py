6a6db31e2661a4d80ccb31153b7a109d856bb374
MattBourgeois <Bourgeoismatthew6@gmail.com>
2023-04-06 21:12:08 -0400

    got perks to print out in string format,

--- Flask_app/models/deep.py
+++ Flask_app/models/deep.py
@@ -10,13 +10,10 @@ class Killer:
 		self.Add_ons = Add_ons
 	
 	def GetKillerPerks(killer_num):
-		m = ""
-		# for i in range(1, 4, 1):
-		# 	x = random.randint(0, 30)
-		# 	Killer.GetKillerPerks(f"{x}", perk_num)
-
-		for perk in data["Players"]["Killers"][killer_num]["Perks"]:
-			m += perk
-		print(m)
+		m = []
+		perk = data["Players"]["Killers"][killer_num]["Perks"]
+		# if perk in data["Players"]["Killers"][killer_num]["Perks"]:
+		m += perk
+		print(m[0])
 		return m
 		# return data["Players"]["Killers"]["0"]["Perk_Description"][perk_num]
