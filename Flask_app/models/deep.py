6edb8a59046942f04377f39b318f4ef04ed5c1aa
MattBourgeois <Bourgeoismatthew6@gmail.com>
2023-04-11 20:40:22 -0400

    adding survivor perks

--- Flask_app/models/deep.py
+++ Flask_app/models/deep.py
@@ -3,10 +3,10 @@ with open("data2.json", "r") as d:
 	data = json.load(d)
 
 class Killer:
-	def __init__(self, Name, Perks, Perk_Description, Add_ons):
+	def __init__(self, Name, Perks, Perk_Descriptions, Add_ons):
 		self.Name = Name
 		self.Perks = Perks
-		self.Perk_description = Perk_Description
+		self.Perk_Descriptions = Perk_Descriptions
 		self.Add_ons = Add_ons
 	
 	def GetKillerPerks(killer_num, perk_num):
@@ -19,3 +19,15 @@ class Killer:
 		perks = (data["Players"]["Killers"][killer_num]["Perk_Descriptions"][perk_num])
 		a = perks
 		return a
+
+class Survivor:
+	def __init__(self, Name, Perks, Perk_Descriptions):
+		self.Name = Name
+		self.Pekrs = Perks
+		self.Perk_Description = Perk_Descriptions
+
+	def GetSurvivorPerks(Surv_num, perk_num):
+		m = []
+		perk = data["Players"][Surv_num]["Perks"][perk_num]
+		m = perk
+		return m
