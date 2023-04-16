eeb7d3887eb3ccaec625be3d400e2b7ab8074cbb
MattBourgeois <Bourgeoismatthew6@gmail.com>
2023-04-16 14:14:50 -0400

    cleaing up method

--- Flask_app/models/deep.py
+++ Flask_app/models/deep.py
@@ -30,12 +30,10 @@ class Survivor:
 		m = []
 		perk = data["Players"]["Survivors"][Surv_num]["Perks"][perk_num]
 		m = perk
-		print(m)
 		return m
 
 	def GetSurvivorDesc(Surv_num, perk_num):
 		a = []
 		perks = data["Players"]["Survivors"][Surv_num]["Perk_Descriptions"][perk_num]
 		a = perks
-		print(a)
 		return a
